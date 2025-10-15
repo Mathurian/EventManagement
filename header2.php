b281227cadefc544799337dea8af6867c23e7830
Mathurian <34145134+Mathurian@users.noreply.github.com>
2025-10-15 13:34:19 -0500

    adjust header to include jq library

--- header2.php
+++ header2.php
@@ -14,6 +14,7 @@
     <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
     <link rel="stylesheet" href="yearpicker.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
                                 
  
